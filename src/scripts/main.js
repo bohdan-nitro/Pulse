ba4901554d243379f73d2b612eff3bf363e25e49
Bohdan-Nitro <bogdantrogonsky@gmail.com>
2020-02-22 22:31:50 +0200

    Scripts commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -87,6 +87,43 @@ $(document).ready(function () {
     valideForms("#consultation form");
     valideForms("#order form");
 
+    //Masked Phone
+
     $("input[name=phone]").mask('+7(999)-999-99-99');
+
+//    Reset Form
+
+    $("form").submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            $("#consultation, #order").fadeOut();
+            $(".overlay, #thanks").fadeIn();
+
+
+            $("form").trigger("reset");
+        });
+        return false;
+    });
+
+//    Smooth scroll and pageup
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $(".pageup").fadeIn();
+        } else {
+            $(".pageup").fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function () {
+        let _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
 
 });
